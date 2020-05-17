0708794e912df3100d345ef50d38961e079a1081
Bhanu Pratap <pratap.bhanu3434@gmail.com>
2020-05-17 19:04:49 +0530

    Updated Changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+          backgroundColor: Colors.deepOrange[900],
           appBar: AppBar(
             title: Center(
               child: Text('I Am Poor'),
             ),
             backgroundColor: Colors.grey[800],
           ),
-          body: Image(
-            image: AssetImage('google.png'),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/google.png'),
+            ),
           )),
     ));
